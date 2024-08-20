80d6f47345e64a519644a98aeb7eb38cbffd33b8
emrullahtastan <emrullah69@gmail.com>
2024-08-20 11:43:09 +0300

    add info and button elements

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import {Info} from "./elements/Info";
+import {Button} from "./elements/Button";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          <Info/>
+          <Button/>
       </header>
     </div>
   );
---
+++ src/elements/Info.jsx
@@ -0,0 +1,5 @@
+export const Info = () => {
+    return (
+        <>0</>
+    )
+}
